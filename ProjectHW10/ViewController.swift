3776b90d6d3547180117512c9369a474d5c491b5
Eugenius Morozov <emorozov777@gmail.com>
2021-11-16 21:15:01 +0300

    Initial Commit

---
+++ ProjectHW10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectHW10
+//
+//  Created by Evgeny Morozov on 16.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
